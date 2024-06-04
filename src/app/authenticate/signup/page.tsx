bd6d5abcb95bb3c6b96c27942a86929e724491a2
Neil Doshi <neil.doshi@vts.com>
2024-06-04 15:25:01 +0530

    fix sign up page

--- src/app/authenticate/signup/page.tsx
+++ src/app/authenticate/signup/page.tsx
@@ -1,15 +1,15 @@
 export default function Signup() {
   return (
     <div className="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm">
-      <div className="p-4 sm:p-7">
+      <div className="p-22 sm:p-7">
         <div className="text-center">
           <h1 className="block text-2xl font-bold text-gray-800">Sign up</h1>
-          <p className="mt-2 text-sm text-gray-600">
-            Already have an account?
-            <a className="text-blue-600 decoration-2 hover:underline font-medium" href="../examples/html/signin.html">
+          <div className="flex mt-2">
+            <p className="pr-5 text-sm text-gray-600 dark:text-neutral-400"> Already have a account? </p>
+            <a className="text-blue-600 text-sm decoration-2 hover:underline font-medium dark:text-blue-500" href="../examples/html/signup.html">
               Sign in here
             </a>
-          </p>
+          </div>
         </div>
 
         <div className="mt-5">
