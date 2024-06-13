74ac72f2b933c48bf1dcba484e087723e1463881
Neil Doshi <mail.neildoshi@gmail.com>
2024-06-13 14:22:44 +0530

    fix bugs; make form resoponsive

--- src/app/dashboard/profile/PersonalDetails.tsx
+++ src/app/dashboard/profile/PersonalDetails.tsx
@@ -10,18 +10,20 @@ export default function PersonalDetails() {
         </h6>
       </div>
       {/** form **/}
-      <div className='flex p-4'>
-        <div className='flex w-1/3'>
-          <label className='mt-1 text-gray-500 dark:text-neutral-400'>
+      <div className='grid p-4 sm:grid-cols-1 md:grid-cols-6'>
+        <div className='grid sm:pb-2 md:col-span-2'>
+          <label className='my-1 text-gray-500 dark:text-neutral-400'>
             Salutation
           </label>
         </div>
-        <div className='w-2/3 flex-1'>
-          <input
-            type='text'
-            className='block w-full w-80 rounded-lg border-gray-200 px-4 py-3 text-sm focus:border-blue-500 focus:ring-blue-500 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
-            placeholder='This is placeholder'
-          />
+        <div className='grid md:col-span-4'>
+          <div className='w-80 space-y-3'>
+            <input
+              type='text'
+              className='block w-full rounded-lg border-gray-200 px-4 py-3 text-sm focus:border-blue-500 focus:ring-blue-500 disabled:pointer-events-none disabled:opacity-50 dark:border-neutral-700 dark:bg-neutral-900 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600'
+              placeholder='This is placeholder'
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -245,9 +245,9 @@ export default function Sidebar() {
                   viewBox='0 0 24 24'
                   fill='none'
                   stroke='currentColor'
-                  stroke-width='2'
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
                 >
                   <path d='m18 15-6-6-6 6' />
                 </svg>
@@ -259,9 +259,9 @@ export default function Sidebar() {
                   viewBox='0 0 24 24'
                   fill='none'
                   stroke='currentColor'
-                  stroke-width='2'
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
+                  strokeWidth='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
                 >
                   <path d='m6 9 6 6 6-6' />
                 </svg>
