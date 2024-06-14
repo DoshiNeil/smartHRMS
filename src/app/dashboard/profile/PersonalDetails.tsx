a3e262a91e2ef83d4e08294921a56aa3528c5a36
Neil Doshi <mail.neildoshi@gmail.com>
2024-06-14 18:40:46 +0530

    Added Salutation field to profile form

--- src/app/dashboard/profile/PersonalDetails.tsx
+++ src/app/dashboard/profile/PersonalDetails.tsx
@@ -1,11 +1,15 @@
-import Salutation, { SalutationOptions } from '@@/components/form/Salutation';
+import Salutation, {
+  SalutationOptionType,
+  SalutationOptions,
+} from '@@/components/form/Salutation';
 import { useState } from 'react';
 
 export default function PersonalDetails() {
-
   const [personalDetails, setPersonalDetails] = useState<Object>();
- 
-  const handleSalutationUpdate = (value: SalutationOptions) => setPersonalDetails(prev => ({ ...prev, salutation: value}));
+
+  const handleSalutationUpdate = (value: SalutationOptionType) =>
+    setPersonalDetails((prev) => ({ ...prev, salutation: value }));
+
   return (
     <div className='flex flex-col rounded-xl border bg-white shadow-sm dark:border-neutral-700 dark:bg-neutral-900 dark:shadow-neutral-700/70'>
       <div className='flex flex-col p-4'>
@@ -18,7 +22,10 @@ export default function PersonalDetails() {
       </div>
       {/** form **/}
       <div className='grid p-4 sm:grid-cols-1 md:grid-cols-6'>
-        <Salutation updateSalutation={handleSalutationUpdate} />
+        <Salutation
+          updateSalutation={handleSalutationUpdate}
+          defaultValue={SalutationOptions.mr}
+        />
       </div>
     </div>
   );
--- src/components/form/Salutation/index.tsx
+++ src/components/form/Salutation/index.tsx
@@ -1,34 +1,55 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
-enum options {
-  mr = 'Mr.',
-  mrs = 'Mrs.',
-  ms = 'Ms',
-  dr = 'Dr.',
-}
+export const SalutationOptions = {
+  mr: 'Mr.',
+  mrs: 'Mrs.',
+  ms: 'Ms',
+  dr: 'Dr.',
+} as const;
 
-export type SalutationOptions = options | null;
+export type SalutationOptionType =
+  | (typeof options)[keyof typeof options]
+  | null;
 
 type SalutationProps = {
-  updateSalutation: (value: SalutationOptions) => void;
-  defaultValue?: SalutationOptions;
+  updateSalutation: (value: SalutationOptionType) => void;
+  defaultValue?: SalutationOptionType;
 };
 
 const Salutation: React.FC<SalutationProps> = ({
   updateSalutation,
   defaultValue = null,
 }) => {
- 
-  const [value, setValue ] = useState<SalutationOptions>(defaultValue);
-  
-  const handleSelect = (v:SalutationOptions) => setValue(v);
+  const [value, setValue] = useState<SalutationOptionType>(defaultValue);
+  const handleChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setValue(event.target.value as SalutationOptionType);
+  };
 
-  useEffect(() => (updateSalutation(value)), [value]);
+  useEffect(() => updateSalutation(value), [value]);
 
-
-
-
-  return <></>;
+  return (
+    <>
+      <div className='grid p-4 md:col-span-2'>
+        <label className=''> Salutation </label>
+      </div>
+      <div className='grid p-4 md:col-span-4'>
+        <select
+          value={value as string}
+          onChange={handleChange}
+          className='block w-full rounded-lg border-transparent bg-gray-100 px-4 py-3 pe-9 text-sm focus:border-blue-500 focus:ring-blue-500 disabled:pointer-events-none disabled:opacity-50 lg:w-2/3 dark:border-transparent dark:bg-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600'
+        >
+          <option value=''>Select a salutation for yourself</option>
+          {Object.entries(SalutationOptions).map(([k, v]) => (
+            <option key={k} value={v}>
+              {v}
+            </option>
+          ))}
+        </select>
+      </div>
+    </>
+  );
 };
 
 export default Salutation;
